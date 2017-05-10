858cb1e93bc690522c122c1ca0fc16e6dd938af8
haydnchapman <haydn.chapman@gmail.com>
2017-05-10 13:31:38 +0100

    Implement selection of epsilon value from cross validation data set

--- Week 9/ex8/selectThreshold.m
+++ Week 9/ex8/selectThreshold.m
@@ -11,6 +11,8 @@ bestF1 = 0;
 F1 = 0;
 
 stepsize = (max(pval) - min(pval)) / 1000;
+cvPredictions = zeros(size(yval));
+
 for epsilon = min(pval):stepsize:max(pval)
     
     % ====================== YOUR CODE HERE ======================
@@ -23,17 +25,16 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+    cvPredictions = pval < epsilon;
+    
+    tp = sum((cvPredictions == 1) & (yval == 1));
+    fp = sum((cvPredictions == 1) & (yval == 0));
+    fn = sum((cvPredictions == 0) & (yval == 1));
 
+    prec = tp / (tp + fp);
+    rec = tp / (tp + fn);
 
-
-
-
-
-
-
-
-
-
+    F1 = (2 * prec * rec)/(prec + rec);
 
     % =============================================================
 
